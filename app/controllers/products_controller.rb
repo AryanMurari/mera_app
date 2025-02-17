71be9bdf882a26d4e5fe156cc6c850aa231e8973
AryanMurari <1aryanmurari@gmail.com>
2025-02-17 14:01:41 +0530

    login password steps 11 completed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[ show edit update destroy ]
+  allow_unauthenticated_access only: %i[index show]
+
+  before_action :set_product, only: %i[show edit update destroy]
 
   def index
     @products = Product.all
@@ -38,11 +40,12 @@ class ProductsController < ApplicationController
   end
 
   private
-    def set_product
-      @product = Product.find(params[:id])
-    end
 
-    def product_params
-      params.expect(product: [ :name ])
-    end
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.expect(product: [:name])
+  end
 end

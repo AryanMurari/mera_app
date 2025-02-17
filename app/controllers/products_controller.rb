a6cfbb6d2290a4678bc08d0c7b5a98ad5565ccc8
AryanMurari <1aryanmurari@gmail.com>
2025-02-17 14:41:51 +0530

    now we can add images with new products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -45,7 +45,8 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  # Only allow a list of trusted parameters through.
   def product_params
-    params.expect(product: [:name])
+    params.expect(product: [:name, :description, :featured_image])
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
-    validates :name, presence: true
-  end  
+  has_one_attached :featured_image  # For image upload
+  has_rich_text :description        # For rich text
+  validates :name, presence: true
+end
